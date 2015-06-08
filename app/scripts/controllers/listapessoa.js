c9aa8c52d6b25d054cd4163819a94b11b09365f5
aurelio.costa <aurelio.costa@afferolab.com.br>
2015-06-08 13:10:17 -0300

    inicio listagem de Pessoas

---
+++ app/scripts/controllers/listapessoa.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name escalaAppApp.controller:ListapessoaCtrl
+ * @description
+ * # ListapessoaCtrl
+ * Controller of the escalaAppApp
+ */
+angular.module('escalaAppApp')
+  .controller('ListapessoaCtrl', function ($scope) {
+    $scope.awesomeThings = ['HTML5 Boilerplate','AngularJS', 'Karma'];
+
+  });
