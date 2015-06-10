40bec9fc5832e5fda7381d421d6b930893253917
Arcosta2014 <aureliorezendecosta@hotmail.com>
2015-06-10 02:07:47 -0300

    Salvando Pessoa

--- app/scripts/controllers/PessoaCtrl.js
+++ app/scripts/controllers/PessoaCtrl.js
@@ -8,9 +8,28 @@
  * Controller of the escalaAppApp
  */
 angular.module('escalaAppApp')
-  .controller('PessoaCtrl', ['$scope', 'PessoaService', function ($scope, PessoaService) {
+  .controller('PessoaCtrl', ['$scope', 'PessoaService', '$routeParams', '$location', function ($scope, PessoaService, $routeParams, $location) {
     $scope.awesomeThings = ['HTML5 Boilerplate','AngularJS', 'Karma'];
+    $scope.semana = [
+      {dia:'DOM', escolhido: false},
+      {dia:'SEG', escolhido: false},
+      {dia:'TER', escolhido: false},
+      {dia:'QUA', escolhido: false},
+      {dia:'QUI', escolhido: false},
+      {dia:'SEX', escolhido: false},
+      {dia:'SAB', escolhido: false},];
+
+    var uid = $routeParams.uid || PessoaService.gerarId();
+    $scope.pessoa = PessoaService.buscarPessoaPorId(uid);
+
+    console.log('Pessoas', $scope.pessoa);
+
     $scope.salvar = function () {
+        $scope.pessoa.semana = $scope.semana;
+        $scope.pessoa.uid = uid;
+        console.log($scope.pessoa);
         PessoaService.salvar($scope.pessoa);
     };
+
+
   }]);
--- app/scripts/services/PessoaService.js
+++ app/scripts/services/PessoaService.js
@@ -7,22 +7,31 @@
  * # pessoa
  * Service in the escalaAppApp.
  */
-angular.module('escalaAppApp').service('PessoaService', ['Ref', '$firebase', '$firebaseArray', function (Ref, $firebase, $firebaseArray) {
-  var pessoaRef = '';
-  var gerarId = function (tipo) {
-    return tipo.substr(0,1) + new Date().getTime().toString();
-  };
+angular.module('escalaAppApp').service('PessoaService', ['Ref', '$firebase', '$firebaseArray', '$firebaseObject', function (Ref, $firebase, $firebaseArray, $firebaseObject) {
+  var pessoaRef = Ref.child('pessoa');
 
   return {
+    gerarId : function () {
+      return new Date().getTime().toString();
+    },
+
     salvar: function (pessoa) {
-      pessoa.uid = gerarId(pessoa.funcao);
-      pessoaRef = Ref.child('pessoa/' + pessoa.uid);
-      pessoaRef.set(pessoa);
+        pessoa.$save();
     },
 
     listar: function (){
-      pessoaRef = Ref.child('pessoa');
       return $firebaseArray(pessoaRef);
+    },
+
+    remover: function (p) {
+      $firebaseArray(pessoaRef).$remove(p);
+    },
+
+    buscarPessoaPorId: function (uid) {
+      if(!uid){
+        var pessoa = pessoaRef.child(uid);
+        return pessoa;
+      }
     }
   };
 
