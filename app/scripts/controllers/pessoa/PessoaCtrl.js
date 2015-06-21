bba11ea826407708ed34168549a5021c18ccdd7a
Arcosta2014 <aureliorezendecosta@hotmail.com>
2015-06-21 12:31:04 -0300

    Salvando Impedimentos

--- app/scripts/controllers/pessoa/PessoaCtrl.js
+++ app/scripts/controllers/pessoa/PessoaCtrl.js
@@ -1,79 +1,72 @@
 'use strict';
 
 angular.module('escalaAppApp')
-  .controller('PessoaCtrl', ['$scope', '$routeParams', '$location', 'Ref', '$firebaseObject',
-    function ($scope, $routeParams, $location, Ref, $firebaseObject) {
-      $scope.awesomeThings = ['HTML5 Boilerplate','AngularJS', 'Karma'];
+  .controller('PessoaCtrl', ['$scope', '$routeParams', '$location', 'Ref', '$firebaseObject', 'uiCalendarConfig', '$compile',
+    function ($scope, $routeParams, $location, Ref, $firebaseObject, uiCalendarConfig, $compile) {
+      var uid;
+      $scope.agenda = $scope.agenda || [];
+      $scope.mensagemSucesso = '';
+      $scope.mensagensErro = [];
+      $scope.impedimentos = [];
 
-      $scope.alertOnEventClick = function(){
-        alert(' was clicked ');
+      function montarImpedimentosParaSalvar (jsonsImpedimento) {
+        var datasImpedimento = [];
+        angular.forEach(jsonsImpedimento, function (item) {
+          datasImpedimento.push(item.start.format());
+        });
+        return datasImpedimento;
+      };
+
+      function montarImpedimentosParaCalendario (datasImpedimento) {
+        angular.forEach(datasImpedimento, function (item) {
+          $scope.impedimentos.push({title: 'IMPEDIMENTO', start: moment(item)});
+        });
+      };
+
+      function init () {
+        if($routeParams.uid){
+          $firebaseObject(Ref.child('pessoa').child($routeParams.uid)).$loaded().then(function (pessoa) {
+            $scope.pessoa = pessoa;
+            montarImpedimentosParaCalendario(pessoa.datasIndisponiveis);
+          });
+        } else {
+          uid = new Date().getTime().toString();
+          $scope.pessoa = $firebaseObject(Ref.child('pessoa').child(uid));
+        }
+      };
+
+      init();
+
+      function dayClick (date, jsEvent, view) {
+        var json = {title: 'IMPEDIMENTO', start: date};
+        $scope.impedimentos.push(json);
       };
 
       /* config object */
       $scope.uiConfig = {
         calendar:{
+          lang: 'pt',
+          titleFormat: 'MMMM YYYY',
           height: 450,
           editable: true,
-          dayClick: function(date, jsEvent, view) {
-
-            alert('Clicked on: ' + date.format());
-
-            alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
-
-            alert('Current view: ' + view.name);
-            // change the day's background color just for fun
-            $(this).css('background-color', 'red');
-          },
+          dayClick: dayClick,
+          events : $scope.impedimentos,
           header:{
             left: 'title',
             center: '',
             right: 'today prev,next'
-          },
-          eventClick: $scope.alertOnEventClick,
-          eventDrop: $scope.alertOnDrop,
-          eventResize: $scope.alertOnResize,
-          eventRender: $scope.eventRender
+          }
         }
       };
-
-      $scope.uiConfig.calendar.dayNamesShort = ["DOM", "SEG", "TER", "QUA", "QUI", "SEX", "SAB"];
-      console.log('$scope.uiConfig.calendar', $scope.uiConfig.calendar);
-      $scope.evento = [
-        {title: 'Primeiro Evento',start: new Date()}
-      ];
-
-      $scope.agenda = [];
 
       $(function () {
         $('#abas a:last').tab('show');
       });
 
-      var uid;
-      $scope.mensagemSucesso = '';
-      $scope.mensagensErro = [];
-      $scope.semana = [
-      {dia:'DOM', escolhido: false},
-      {dia:'SEG', escolhido: false},
-      {dia:'TER', escolhido: false},
-      {dia:'QUA', escolhido: false},
-      {dia:'QUI', escolhido: false},
-      {dia:'SEX', escolhido: false},
-      {dia:'SAB', escolhido: false},];
-
-      if($routeParams.uid){
-        $firebaseObject(Ref.child('pessoa').child($routeParams.uid)).$loaded().then(function (pessoa) {
-          $scope.pessoa = pessoa;
-          $scope.semana = pessoa.semana;
-        });
-      } else {
-        uid = new Date().getTime().toString();
-        $scope.pessoa = $firebaseObject(Ref.child('pessoa').child(uid));
-      }
-
       $scope.salvar = function () {
         $scope.mensagemSucesso = '';
         $scope.mensagensErro = [];
-        $scope.pessoa.semana = $scope.semana;
+        $scope.pessoa.datasIndisponiveis = montarImpedimentosParaSalvar($scope.impedimentos);
           if(uid) {
             $scope.pessoa.uid = uid;
           }
