9a6ab84ca50ca9faeeb6b04ba34f422f6a654f52
aurelio.costa <aurelio.costa@afferolab.com.br>
2015-07-02 08:54:26 -0300

    mensagens de validação

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -37,6 +37,7 @@ module.exports = function(config) {
       'bower_components/moment/moment.js',
       'bower_components/fullcalendar/dist/fullcalendar.js',
       'bower_components/angular-ui-calendar/src/calendar.js',
+      'bower_components/angular-bootstrap-datetimepicker/src/js/datetimepicker.js',
       'bower_components/mockfirebase/browser/mockfirebase.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
